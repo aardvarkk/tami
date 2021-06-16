62955882601a0767502eae6b212d11b872738355
Ian Clarkson <clarksonian@gmail.com>
2021-06-15 22:34:36 -0700

    These __int64 types didn't seem to exist, so cut them out for now

--- Source/Common.h
+++ Source/Common.h
@@ -23,11 +23,11 @@
 typedef unsigned char		uint8;
 typedef unsigned short		uint16;
 typedef unsigned long		uint32;
-typedef unsigned __int64	uint64;
+//typedef unsigned __int64	uint64;
 typedef signed char			int8;
 typedef signed short		int16;
 typedef signed long			int32;
-typedef signed __int64		int64;
+//typedef signed __int64		int64;
 
 #define _MAIN_H_
 
