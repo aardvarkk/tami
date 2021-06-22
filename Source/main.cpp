a553398cfce9df4141bb28ac18217af19532ef91
Ian Clarkson <clarksonian@gmail.com>
2021-06-21 21:31:17 -0700

    Add FTXUI and rename to tami

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+#include "ftxui/dom/elements.hpp"
+#include "ftxui/screen/screen.hpp"
+#include "ftxui/screen/string.hpp"
+
 #include "APU/APU.h"
 
 class App : public IAudioCallback {
@@ -9,10 +13,38 @@ class App : public IAudioCallback {
 };
 
 int main() {
-  std::cout << "hi" << std::endl;
-
   auto apu = new CAPU(new App(), nullptr);
   apu->SetupSound(44100, 2, MACHINE_NTSC);
+
+  using namespace ftxui;
+
+  auto summary = [&] {
+    auto content = vbox({
+                          hbox({text(L"- done:   "), text(L"3") | bold}) | color(Color::Green),
+                          hbox({text(L"- active: "), text(L"2") | bold}) | color(Color::RedLight),
+                          hbox({text(L"- queue:  "), text(L"9") | bold}) | color(Color::Red),
+                        });
+    return window(text(L" Summary "), content);
+  };
+
+  auto document =  //
+    vbox({
+           hbox({
+                  summary(),
+                  summary(),
+                  summary() | flex,
+                }),
+           summary(),
+           summary(),
+         });
+
+  // Limit the size of the document to 80 char.
+  document = document | size(WIDTH, LESS_THAN, 80);
+
+  auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
+  Render(screen, document);
+
+  std::cout << screen.ToString() << std::endl;
 
   return EXIT_SUCCESS;
 }
